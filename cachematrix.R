9b646077adec9421fa30279cac99b0ff3f849e98
NewtoRprog <erankitdaviet@gmail.com>
2016-03-17 22:53:57 +0530

    Update cachematrix.R
    updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,19 @@
 ##a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will
 ##not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
-## This function will help in making a matrix and then returning inverse of it.
+## This function will help in making a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv<- NULL
 ## set function helps in assigning a matrix to x through y. It can accept either
 ## 1X1 matrix or 2X2 or 3X3 similarly nXn
+
 	set <- function(y){
 		x <<- y
 		inv <<- NULL
 	}
-##get function will display the matrix which was given by user as argument
-##set Function
+##Get function will display the matrix which was given by user as argument set function.
+
 	get <- function() x
 	setinverse <- function(inverse) inv <<- inverse
 	getinverse <- function() inv
@@ -24,19 +25,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## this will create and return inverse of matrix which is created in above function
+## This will create and return inverse of matrix which is created in above function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	inv <- x$getinverse()
-## this will check if inv is not null, already existing data then goes into loop.
+## This will check if inv is not null, already existing data then goes into loop.
 	if(!is.null(inv)){
-		message("getting cached data")
+		message("cached data")
 		return(inv)
 	}
-##set matri equals to matrix which was entered in first function
+## Set matri equals to matrix which was entered in first function
 	matri <-x$get()
-##create inverse of matri
+## Create inverse of matri
 	inv <- solve(matri, ...)
 	x$setinverse(inv)
 	inv
